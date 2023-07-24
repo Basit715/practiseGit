8e96394d4cf056985ce152df878c406c527ff074
Basit715 <basitabass27411@gmail.com>
2023-07-24 13:36:55 +0530

    class Person

---
+++ func.py
@@ -0,0 +1,12 @@
+class Person:
+     def __init__(self, name, age):
+          self.name = name
+          self.age = age
+          
+     def show(self):
+          print(f"The name is {self.name}")
+          print(f"The age is {self.age}")
+          
+          
+p = Person("Basit", 25)
+p.show() 
